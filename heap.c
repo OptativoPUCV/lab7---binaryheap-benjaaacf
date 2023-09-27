d5629ff4d73c3025eef4b310634b70e777a7c437
Replit user <>
2023-09-27 21:51:24 +0000

    new try

--- heap.c
+++ heap.c
@@ -1,38 +1,30 @@
+#include "heap.h"
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include "heap.h"
 
-typedef struct nodo{
-   void* data;
-   int priority;
-}heapElem;
+typedef struct nodo {
+  void *data;
+  int priority;
+} heapElem;
 
-typedef struct Heap{
-  heapElem* heapArray;
+typedef struct Heap {
+  heapElem *heapArray;
   int size;
   int capac;
 } Heap;
 
-
-void* heap_top(Heap* pq){
+void *heap_top(Heap *pq) {
+  if (pq->size != 0) {
     return NULL;
+  }
+  return (void)pq->heapArray { 0 }
 }
 
+void heap_push(Heap *pq, void *data, int priority) {}
 
+void heap_pop(Heap *pq) {}
 
-void heap_push(Heap* pq, void* data, int priority){
-
-}
-
-
-void heap_pop(Heap* pq){
-
-}
-
-Heap* createHeap(){
-
-   return NULL;
-}
+Heap *createHeap() { return NULL; }
