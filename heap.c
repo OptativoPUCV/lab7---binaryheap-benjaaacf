18158a16197f5be7fc6dcfa443cf381fbdfeb193
Replit user <>
2023-10-06 01:20:03 +0000

    new try

--- heap.c
+++ heap.c
@@ -54,11 +54,10 @@ void heap_push(Heap *pq, void *data, int priority) {
 
 void heap_pop(Heap *pq) {}
 
-Heap *createHeap() 
-{
-  Heap* hp = (Heap*)malloc(sizeof(Heap));
+Heap *createHeap() {
+  Heap *hp = (Heap *)malloc(sizeof(Heap));
 
-  hp -> capac = 3;
-  hp -> size = 0;
-  hp -> heapArray = (heapElem*)malloc(3*sizeof(heapElem);
+  hp->capac = 3;
+  hp->size = 0;
+  hp->heapArray = (heapElem *)malloc(3 * sizeof(heapElem));
 }
